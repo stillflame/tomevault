820bb3c683375381775eb2fa31b9635d0339c7a0
stillflame <idawoodjee@mac.com>
2025-06-25 20:02:53 -0400

    new enum

---
+++ app/Enums/ArtifactType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Enums;
+
+enum ArtifactType: string
+{
+    case Grimoire = 'Grimoire';
+    case Scroll = 'Scroll';
+    case Codex = 'Codex';
+    case Tome = 'Tome';
+    case Manuscript = 'Manuscript';
+    case Tablet = 'Tablet';
+    case Pamphlet = 'Pamphlet';
+
+    case Compendium = 'Compendium';
+    case Diary = 'Diary';
+    case Encyclopedia = 'Encyclopedia';
+    case Journal = 'Journal';
+    case Ledger = 'Ledger';
+    case ScrollCase = 'Scroll Case';
+    case Spellbook = 'Spellbook';
+    case Treatise = 'Treatise';
+    case Vellum = 'Vellum';
+    case Codicil = 'Codicil';
+    case Atlas = 'Atlas';
+    case Almanac = 'Almanac';
+    case Missal = 'Missal';
+    case Papyrus = 'Papyrus';
+    case Archive = 'Archive';
+    case Bestiary = 'Bestiary';
+    case Chronicle = 'Chronicle';
+    case Fascicle = 'Fascicle';
+    case Liber = 'Liber';
+}
---
+++ app/Enums/CoverMaterial.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Enums;
+
+enum CoverMaterial: string
+{
+    case Leather = 'Leather';
+    case Dragonhide = 'Dragonhide';
+    case Velvet = 'Velvet';
+    case Cloth = 'Cloth';
+    case Parchment = 'Parchment';
+    case Wood = 'Wood';
+    case GoldLeaf = 'Gold Leaf';
+    case SilverLeaf = 'Silver Leaf';
+    case Bone = 'Bone';
+    case Iron = 'Iron';
+    case Bronze = 'Bronze';
+    case Glass = 'Glass';
+    case EnchantedSilk = 'Enchanted Silk';
+    case Obsidian = 'Obsidian';
+    case Crystal = 'Crystal';
+    case Marble = 'Marble';
+    case Papyrus = 'Papyrus';
+    case Silk = 'Silk';
+    case VelvetEmbroidered = 'Velvet Embroidered';
+    case RuneInscribedLeather = 'Rune-Inscribed Leather';
+    case ShadowwovenCloth = 'Shadowwoven Cloth';
+    case WyvernScale = 'Wyvern Scale';
+    case PhantomFabric = 'Phantom Fabric';
+    case Emberwood = 'Emberwood';
+    case CelestialWeave = 'Celestial Weave';
+    case DragonScale = 'Dragon Scale';
+    case Mithril = 'Mithril';
+    case EtherealLinen = 'Ethereal Linen';
+    case LeatherBound = 'Leather Bound';
+    case FossilizedWood = 'Fossilized Wood';
+    case BasiliskHide = 'Basilisk Hide';
+    case SpellwovenCloth = 'Spellwoven Cloth';
+}
