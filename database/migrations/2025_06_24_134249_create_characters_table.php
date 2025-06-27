42ebc048d589748f6722c284b6c4a4c15016b55a
stillflame <idawoodjee@mac.com>
2025-06-27 13:54:24 -0400

    Update 2025_06_24_134249_create_characters_table.php

--- database/migrations/2025_06_24_134249_create_characters_table.php
+++ database/migrations/2025_06_24_134249_create_characters_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('characters', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->string('slug')->nullable()->unique()->after('name');
+            $table->string('slug')->nullable()->unique();
             $table->text('bio')->nullable();
             $table->timestamps();
         });
